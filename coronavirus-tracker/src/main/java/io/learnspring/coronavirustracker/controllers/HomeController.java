29cf711f72f9ffb251136c845ed0437e76f8dfea
sandesh611 <123733928+sandesh611@users.noreply.github.com>
2023-09-29 12:17:31 +0530

    Add files via upload

---
+++ coronavirus-tracker/src/main/java/io/learnspring/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,31 @@
+package io.learnspring.coronavirustracker.controllers;
+
+
+import io.learnspring.coronavirustracker.models.LocationStats;
+import io.learnspring.coronavirustracker.service.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+    @GetMapping("/")
+    public String home(Model model) {
+
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(diff -> diff.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases",totalReportedCases);
+        model.addAttribute("totalNewCases",totalNewCases);
+
+
+        return "home";
+    }
+}
---
+++ coronavirus-tracker/src/main/java/io/learnspring/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,55 @@
+package io.learnspring.coronavirustracker.models;
+
+public class LocationStats {
+
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int diffFromPrevDay;
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+
+
+
+    public String getCountry() {
+        return country;
+    }
+
+
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    @Override
+    public String toString()
+    {
+        return getClass().getSimpleName() + "["+ state  + ","+ country + "," + latestTotalCases + "]";
+    }
+
+}
